95c45c0e6276ddbc171b79a1c2972419d71c7580
leriel <zimorod@gmail.com>
2017-01-04 20:39:48 +0100

    update webpack to build to `dist`

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: 'js/[name].[chunkhash:8].js',
   },
   plugins: [
